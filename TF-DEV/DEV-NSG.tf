73ac3a64901a600d4ff6382e268ade7378c8385b
Administrator@ENFRASYS.LOCAL <Administrator@ENFRASYS.LOCAL>
2019-08-28 09:48:28 +0800

    DEV UPDATED

--- TF-DEV/DEV-NSG.tf
+++ TF-DEV/DEV-NSG.tf
@@ -1,5 +1,25 @@
-/*resource "azurerm_network_security_group" "NSG-APP" {
-  name                = "NSG-APP"
+#/*
+resource "azurerm_network_security_group" "NSG-APP-DEV" {
+  name                = "NSG-APP-DEV"
+  location            = "${azurerm_resource_group.DEV.location}"
+  resource_group_name = "${azurerm_resource_group.DEV.name}"
+
+  security_rule {
+    name                       = "AllowRDPFromALL"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+
+  }
+  }
+
+  resource "azurerm_network_security_group" "NSG-WEB-DEV" {
+  name                = "NSG-WEB-DEV"
   location            = "${azurerm_resource_group.PROD.location}"
   resource_group_name = "${azurerm_resource_group.PROD.name}"
 
@@ -17,27 +37,8 @@
   }
   }
 
-  resource "azurerm_network_security_group" "NSG-WEB" {
-  name                = "NSG-WEB"
-  location            = "${azurerm_resource_group.PROD.location}"
-  resource_group_name = "${azurerm_resource_group.PROD.name}"
-
-  security_rule {
-    name                       = "AllowRDPFromALL"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-
-  }
-  }
-
-   resource "azurerm_network_security_group" "NSG-DB" {
-  name                = "NSG-DB"
+   resource "azurerm_network_security_group" "NSG-DB-DEV" {
+  name                = "NSG-DB-DEV"
   location            = "${azurerm_resource_group.PROD.location}"
   resource_group_name = "${azurerm_resource_group.PROD.name}"
 
@@ -54,4 +55,4 @@
 
   }
   }
-  */
+  #*/
--- TF-DEV/DEV.tf
+++ TF-DEV/DEV.tf
@@ -1,14 +1,15 @@
 
-/*provider "azurerm" {
+#/*
+provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
   version = "=1.28.0"
   #prodsubcription
-   subscription_id = "ee31172f-3e56-4f55-94e8-6adce8c23e83" 
+   subscription_id = "7accd93c-0ecb-4767-aabc-2b856d78704a" 
 }
 
 # Create a resource group
-resource "azurerm_resource_group" "PROD" {
-  name     = "PROD-RG"
+resource "azurerm_resource_group" "DEV" {
+  name     = "DEV-RG"
   location = "Southeast Asia"
   tags = {
         environment = "Terraform"
@@ -17,38 +18,38 @@ resource "azurerm_resource_group" "PROD" {
 
 # Create a virtual network within the resource group
 
-resource "azurerm_virtual_network" "PROD-vNet" {
-    name                = "PROD-vNET"
-    address_space       = ["192.168.1.0/24"]
+resource "azurerm_virtual_network" "DEV-vNet" {
+    name                = "DEV-vNET"
+    address_space       = ["192.168.3.0/24"]
     location            = "Southeast asia"
-    resource_group_name = "${azurerm_resource_group.PROD.name}"
+    resource_group_name = "${azurerm_resource_group.DEV.name}"
     tags = {
         environment = "Terraform"
     }
 }
 
 resource "azurerm_subnet" "prod-frondend-subnet" {
-    name                 = "PROD-WEB-SUBNET"
-    resource_group_name  = "${azurerm_resource_group.PROD.name}"
-    virtual_network_name = "${azurerm_virtual_network.PROD-vNet.name}"
-    address_prefix       = "192.168.1.0/28"
-    network_security_group_id ="${azurerm_network_security_group.NSG-WEB.id}"
+    name                 = "DEV-WEB-SUBNET"
+    resource_group_name  = "${azurerm_resource_group.DEV.name}"
+    virtual_network_name = "${azurerm_virtual_network.DEV-vNet.name}"
+    address_prefix       = "192.168.3.0/28"
+    network_security_group_id ="${azurerm_network_security_group.NSG-WEB-DEV.id}"
 }
 
 resource "azurerm_subnet" "prod-app-subnet" {
     name                 = "PROD-APP-SUBNET"
     resource_group_name  = "${azurerm_resource_group.PROD.name}"
     virtual_network_name = "${azurerm_virtual_network.PROD-vNet.name}"
-    address_prefix       = "192.168.1.16/28"
-    network_security_group_id ="${azurerm_network_security_group.NSG-APP.id}"
+    address_prefix       = "192.168.3.16/28"
+    network_security_group_id ="${azurerm_network_security_group.NSG-APP-DEV.id}"
 
 }
-resource "azurerm_subnet" "prod-DB-subnet" {
-    name                 = "PROD-DB-SUBNET"
-    resource_group_name  = "${azurerm_resource_group.PROD.name}"
-    virtual_network_name = "${azurerm_virtual_network.PROD-vNet.name}"
-    address_prefix       = "192.168.1.32/28"
-    network_security_group_id ="${azurerm_network_security_group.NSG-DB.id}"
+resource "azurerm_subnet" "dev-DB-subnet" {
+    name                 = "DEV-DB-SUBNET"
+    resource_group_name  = "${azurerm_resource_group.DEV.name}"
+    virtual_network_name = "${azurerm_virtual_network.DEV-vNet.name}"
+    address_prefix       = "192.168.3.32/28"
+    network_security_group_id ="${azurerm_network_security_group.NSG-DB-DEV.id}"
 }
 /*
 resource "azurerm_subnet" "prod-PRODSVC-subnet" {
